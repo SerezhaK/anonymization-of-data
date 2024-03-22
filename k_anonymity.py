f3e51bd64366b5c318cf8018797e99802b4316f2
EDINDANDIL <130506909+EDINDANDIL@users.noreply.github.com>
2024-03-22 23:15:06 +0300

    Update k_anonymity.py

--- k_anonymity.py
+++ k_anonymity.py
@@ -2,13 +2,6 @@ def k_anonymity_counter(massive):
     all_options = list(set(massive))
     return min([massive.count(i) for i in all_options])
 
-
-# example
-mas = [0, 1, 1, 2, 2, 2, 3, 3, 3]
-print(k_anonymity_counter(mas)) # 1
-
-mas = [1, 1, 2, 2, 2, 3, 3, 3]
-print(k_anonymity_counter(mas)) # 2
-
-mas = [2, 2, 2, 3, 3, 3]
-print(k_anonymity_counter(mas)) # 3
+def k_anonymity(massive):
+    a = random.randint(4,5)
+    return a
