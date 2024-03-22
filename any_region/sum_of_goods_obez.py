eb8b6271b58d6a46dafe7689d1cc34f57dd4b280
EDINDANDIL <130506909+EDINDANDIL@users.noreply.github.com>
2024-03-22 20:38:04 +0300

    Update sum_of_goods_obez.py

--- any_region/sum_of_goods_obez.py
+++ any_region/sum_of_goods_obez.py
@@ -12,18 +12,16 @@ def sum_goods_generation(cnt):
 # Обезличивание
 def obez_sum_goods(sum_goods):
     obez_sum_goods = []
-    for x in range(len(sum_goods)):
-        if 1 <= sum_goods[x] <= 5:
-            obez_sum_goods.append('От 1 до 5 товаров')
-        elif 6 <= sum_goods[x] <= 10:
-            obez_sum_goods.append('От 6 до 10 товаров')
-        elif 11 <= sum_goods[x] <= 15:
-            obez_sum_goods.append('От 11 до 15 товаров')
-        elif 16 <= sum_goods[x] <= 20:
-            obez_sum_goods.append('От 16 до 20 товаров')
-        elif sum_goods[x] > 20:
-            obez_sum_goods.append('Более 20 товаров')
+    if 1 <= sum_goods <= 5:
+        obez_sum_goods.append('От 1 до 5 товаров')
+    elif 6 <= sum_goods <= 10:
+        obez_sum_goods.append('От 6 до 10 товаров')
+    elif 11 <= sum_goods <= 15:
+        obez_sum_goods.append('От 11 до 15 товаров')
+    elif 16 <= sum_goods <= 20:
+        obez_sum_goods.append('От 16 до 20 товаров')
+    elif sum_goods > 20:
+        obez_sum_goods.append('Более 20 товаров')
     return obez_sum_goods
-  
 
 
